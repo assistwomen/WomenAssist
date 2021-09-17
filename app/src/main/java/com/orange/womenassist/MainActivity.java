1e0563503e7ef0918ecca663eb92f7aebd6685f4
LA COREENNE <kuigouareine0@gmail.com>
2021-09-17 18:27:38 +0200

    Connexion

--- app/src/main/java/com/orange/womenassist/MainActivity.java
+++ app/src/main/java/com/orange/womenassist/MainActivity.java
@@ -1,17 +1,13 @@
 package com.orange.womenassist;
 
-<<<<<<< HEAD
-
 import android.content.Intent;
-=======
->>>>>>> c91f4843a7d0493e96d533d891eb66ae81d9ce51
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     
