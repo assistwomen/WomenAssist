89cb82dcd153ab4fbb24422ea339131784e2e2aa
LA COREENNE <kuigouareine0@gmail.com>
2021-09-17 14:57:04 +0200

    Modif envoie alex

--- app/src/main/java/com/orange/womenassist/MainActivity.java
+++ app/src/main/java/com/orange/womenassist/MainActivity.java
@@ -1,13 +1,32 @@
 package com.orange.womenassist;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button connexionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        connexionButton = findViewById(R.id.connexionBtn);
+
+        connexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ConnexionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
