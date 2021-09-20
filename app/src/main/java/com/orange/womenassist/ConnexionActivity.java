9f95356e3fad5cae24a09f12cc0cb830d93a00a8
LA COREENNE <kuigouareine0@gmail.com>
2021-09-20 09:47:39 +0200

    Interface splasgScreen, navigation retour

--- app/src/main/java/com/orange/womenassist/ConnexionActivity.java
+++ app/src/main/java/com/orange/womenassist/ConnexionActivity.java
@@ -3,12 +3,26 @@ package com.orange.womenassist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ConnexionActivity extends AppCompatActivity {
+
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+        getSupportActionBar().hide();
+
+        backButton = findViewById(R.id.backBtn);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/orange/womenassist/MainActivity.java
+++ app/src/main/java/com/orange/womenassist/MainActivity.java
@@ -3,6 +3,7 @@ package com.orange.womenassist;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
 
         connexionButton = findViewById(R.id.connexionBtn);
 
