fbc8dc2e1000cf660f843b36411706653efae084
Tiowa <suprem3479@gmail.com>
2021-09-21 12:18:23 +0100

    interfaces

---
+++ app/src/main/java/com/orange/womenassist/AfficheAssociation.java
@@ -0,0 +1,14 @@
+package com.orange.womenassist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfficheAssociation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affiche_association);
+    }
+}
