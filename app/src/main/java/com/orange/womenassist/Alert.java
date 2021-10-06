b18ac2339b3a402c6988934de97c7e3877c3904b
Tiowa <suprem3479@gmail.com>
2021-10-06 14:31:57 +0100

    nouvelle interface

---
+++ app/src/main/java/com/orange/womenassist/Alert.java
@@ -0,0 +1,14 @@
+package com.orange.womenassist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Alert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+    }
+}
